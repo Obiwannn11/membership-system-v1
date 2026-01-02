303466d671581c6619cbbefd1056959f0e4a7512
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-01-03 05:13:59 +0800

    menambahkan js untuk efek dari sonner

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'; // Tambahkan Pinia sekalian
 import router from './router';      // Tambahkan Router
 import App from './App.vue';
 
+import 'vue-sonner/style.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
