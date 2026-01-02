18e3449c5f40f9c5cee21f99100fc62e8f926b78
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-01-02 17:37:22 +0800

    menambahkan config untuk vue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -9,7 +11,20 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'), // Alias @ menunjuk ke folder JS Laravel
+        },
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
