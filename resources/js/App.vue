dde4df1d78d0eb9eb49b05ae0f58498619517577
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-01-03 20:11:51 +0800

    menambahkan maksimal sonner 4, auto expand, dan memperbaiki tulisan loading

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,13 +3,13 @@
   <div v-if="authStore.loadingUser" class="flex h-screen items-center justify-center bg-slate-50">
     <div class="flex flex-col items-center gap-2">
       <div class="h-8 w-8 animate-spin rounded-full border-4 border-slate-900 border-t-transparent"></div>
-      <p class="text-sm text-slate-500 font-medium">Memuat User...</p>
+      <p class="text-sm text-slate-500 font-medium">Loading</p>
     </div>
   </div>
   
   <template v-else>
     <router-view></router-view>
-    <Toaster position="top-center" richColors />
+    <Toaster position="top-center" richColors :expand="true" :visibleToasts="3" />
   </template>
 </template>
 
