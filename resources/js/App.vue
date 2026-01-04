618a2852152941dd765353ccb1c4ec2858073b87
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-01-05 02:36:56 +0800

    menambahkan component tooltip dan memasukkan ke dalam app vue agar global

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,22 +1,27 @@
 <template>
+  <Toaster position="top-center" richColors :expand="true" :visibleToasts="3" />
   
-  <div v-if="authStore.loadingUser" class="flex h-screen items-center justify-center bg-slate-50">
-    <div class="flex flex-col items-center gap-2">
-      <div class="h-8 w-8 animate-spin rounded-full border-4 border-slate-900 border-t-transparent"></div>
-      <p class="text-sm text-slate-500 font-medium">Loading</p>
-    </div>
+    <TooltipProvider >
+
+      <div v-if="authStore.loadingUser" class="flex h-screen items-center justify-center bg-slate-50">
+        <div class="flex flex-col items-center gap-2">
+          <div class="h-8 w-8 animate-spin rounded-full border-4 border-slate-900 border-t-transparent"></div>
+          <p class="text-sm text-slate-500 font-medium">Loading</p>
+        </div>
   </div>
   
   <template v-else>
     <router-view></router-view>
-    <Toaster position="top-center" richColors :expand="true" :visibleToasts="3" />
+    
   </template>
+</TooltipProvider>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import { useAuthStore } from '@/stores/auth';
 import { Toaster } from 'vue-sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 const authStore = useAuthStore();
 
